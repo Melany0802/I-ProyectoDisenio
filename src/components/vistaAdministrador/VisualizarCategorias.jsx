ba65420222c9a8cbc5a794965f41cd7dfed365c2
Melany0802 <melcris0802@estudiantec.cr>
2024-03-03 17:26:36 -0600

    Category visualization.

--- src/components/vistaAdministrador/VisualizarCategorias.jsx
+++ src/components/vistaAdministrador/VisualizarCategorias.jsx
@@ -1,13 +1,41 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react"
 import Header from "../Header";
 
-function VisualizarCategorias(){
-    return(
+function VisualizarCategorias() {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+
+        getAllCategories();
+    });
+
+    const getAllCategories = async () => {
+        try {
+            const response = await fetch(`https://api.escuelajs.co/api/v1/categories`);
+            const data2 = await response.json();
+            setCategories(data2);
+        } catch (error) {
+            console.error('Error:', error);
+            toast.error('Hubo un error al cargar las categorías');
+        }
+    }
+    return (
         <div>
             <div>
-            <div className=" border border-orange-600">
-                <Header />
+                <div className=" border border-orange-600">
+                    <Header />
+
                 </div>
+                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center">
+                    {categories.map((category) => (
+                        <li key={category.id} className="p-4 flex">
+                            <div className="bg-white rounded p-6 shadow ">
+                                <h2 className="text-xl font-bold mb-2">{category.name}</h2>
+                                <img src={category.image} className="h-48 object-cover mb-4 rounded-lg" />
+                            </div>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
